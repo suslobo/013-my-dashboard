d857119c74733912b90b28509cbd04142cbe3834
slobo <suslobo@hotmail.es>
2024-09-12 13:00:00 +0200

    Configuración del dashboard - Tailwind

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
@@ -8,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './sidemenu.component.css'
 })
 export class SidemenuComponent {
+
+  //nos creamos una nueva propiedad para las rutas
+  public menuItems = routes
+    .map( route => route.children ?? [] )
+    .flat()
+    //si la ruta existe, la ruta tiene path
+    .filter( route => route && route.path )
+    //excluímos si el path tiene :
+    .filter( route => !route.path?.includes(':') );
+
+  constructor() {
+
+    //las rutas que queremos que sean parte de mi menú
+    const dashboardRoutes = routes
+      // .map( route => route.children ?? [] )
+      // .flat()
+      // //si la ruta existe, la ruta tiene path
+      // .filter( route => route && route.path )
+      // //excluímos si el path tiene :
+      // .filter( route => !route.path?.includes(':') );
+
+      console.log(dashboardRoutes);
+  }
 
 }
