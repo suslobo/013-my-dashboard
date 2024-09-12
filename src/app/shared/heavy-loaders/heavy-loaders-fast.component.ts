2e48e91dd9ffb6ab2605254645e4a35af426f6f0
slobo <suslobo@hotmail.es>
2024-09-12 10:42:55 +0200

    Creación de Archivos, directorios y Componentes

---
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-fast',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+  <h1>Hola Mundo</h1>`,
+})
+export class HeavyLoadersFastComponent {
+
+}
