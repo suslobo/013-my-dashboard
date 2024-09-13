528dbe2531d15d28f43c7d6c9b61674074829fb4
slobo <suslobo@hotmail.es>
2024-09-13 13:21:48 +0200

    Defer Blocks

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+//la idea de este componente es que sea un componente bloqueante
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
   imports: [CommonModule],
   template: `
-  <h1>Hola Mundo</h1>`,
+    <section [ngClass]="['w-full h-[600px]', cssClas]">
+      Heavy Loader Slow
+    </section>
+  `,
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({ required: true }) cssClas!: string;
+
+
+
+//estructura para el código bloqueante, esto en la vida real no se hace
+  constructor() {
+    //esto bloquea javascript
+    //durante 3 segundos la app está bloqueada
+    const star = Date.now();
+    while(Date.now() - star < 3000) {}
+  }
 
 }
