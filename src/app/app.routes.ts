0813c50546deb5629a18f0570a2cf862ae97d670
slobo <suslobo@hotmail.es>
2024-09-19 11:41:50 +0200

    Corrección del input y output

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,58 +3,76 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'dashboard',
-    loadComponent: () => import('./dashboard/dashboard.component'),
+    loadComponent: () =>
+      import('./dashboard/dashboard.component'),
     //rutas hijas
     children:[
       {
         path: 'change-detection',
         title: 'Change Detection',
-        loadComponent: () => import('./dashboard/pages/change-detection/change-detection.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/change-detection/change-detection.component'),
       },
       {
         path: 'control-flow',
         title: 'Control Flow',
-        loadComponent: () => import('./dashboard/pages/control-flow/control-flow.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/control-flow/control-flow.component'),
       },
       {
         path: 'defer-options',
         title: 'Defer Options',
-        loadComponent: () => import('./dashboard/pages/defer-options/defer-options.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/defer-options/defer-options.component'),
       },
       {
         path: 'defer-views',
         title: 'Defer Views',
-        loadComponent: () => import('./dashboard/pages/defer-views/defer-views.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/defer-views/defer-views.component'),
       },
       {
         path: 'user/:id',
         title: 'User View',
-        loadComponent: () => import('./dashboard/pages/user/user.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/user/user.component'),
       },
       {
         path: 'user-list',
         title: 'User List',
-        loadComponent: () => import('./dashboard/pages/users/users.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/users/users.component'),
       },
       {
         path: 'view-transition-1',
         title: 'View Transition 1',
-        loadComponent: () => import('./dashboard/pages/view-transition/view-transition1.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/view-transition/view-transition1.component'),
       },
       {
         path: 'view-transition-2',
         title: 'View Transition 2',
-        loadComponent: () => import('./dashboard/pages/view-transition/view-transition2.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/view-transition/view-transition2.component'),
       },
       {
         path: 'inputs-outputs',
         title: 'Inputs Outputs',
-        loadComponent: () => import('./dashboard/pages/input-output/input-output.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/input-output/input-output.component'),
         //para que funcione tenemos que ir al componente y poner default en el export default class InputOutputComponent
       },
-
       {
-        path: '', redirectTo: 'control-flow', pathMatch:'full',
+        path: 'material',
+        title: 'Angular Material',
+        loadComponent: () =>
+          import('./dashboard/pages/material/material.component'),
+        //para que funcione tenemos que ir al componente y poner default en el export default class InputOutputComponent
+      },
+      {
+        path: '',
+        redirectTo: 'control-flow',
+        pathMatch:'full',
       }
     ]
   },
--- src/app/dashboard/pages/input-output/ui/product-card/product-card.component.ts
+++ src/app/dashboard/pages/input-output/ui/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, input, Input, output, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, EventEmitter, input, Input, output, Output } from '@angular/core';
 import { Product } from '@interfaces/product.interface';
 
 @Component({
@@ -28,4 +28,8 @@ export class ProductCardComponent {
   public incrementQuantity(): void {
     this.onIncrementQuantity.emit(this.product().quantity + 1);
   }
+
+  // public loginEffect = effect(() => {
+  //   console.log(this.product().name);
+  // });
 }
