1b2c2f33121b3e6441d278e6ca48b123695de1ef
slobo <suslobo@hotmail.es>
2024-09-19 12:12:50 +0200

    Usar componente de Angular Material

--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -1,10 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+
 
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule],
   templateUrl: './material.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
